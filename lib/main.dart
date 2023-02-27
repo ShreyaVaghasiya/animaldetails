8cf065117e0577f98136a69390846740f8626c5c
ShreyaVaghasiya <shreyavaghasiya026@gmail.com>
2023-02-27 08:36:52 +0530

    SV

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:flutter/services.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
 import 'SplashScreen.dart';
+
 Future<void> copyDatabase() async {
   // Get the directory for storing the database file
   Directory documentsDirectory = await getApplicationDocumentsDirectory();
@@ -18,12 +21,12 @@ Future<void> copyDatabase() async {
     // If the file doesn't exist, copy it from the assets folder
     ByteData data = await rootBundle.load(join('assets/images/Animals.db'));
     List<int> bytes =
-    data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
     await File(path).writeAsBytes(bytes);
   }
 }
-void main()
-async{
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await copyDatabase();
@@ -31,18 +34,15 @@ async{
   String path = join(documentsDirectory.path, 'Animals.db');
   Database database = await openDatabase(path);
 
-  var list = await database.rawQuery('SELECT * FROM Animals');
+  var list = await database.rawQuery('SELECT * FROM animalTable');
 
   for (var row in list) {
     print(row);
   }
-  runApp(
-     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/' : (context) => const MyApp(),
-      },
-    )
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/': (context) => const MyApp(),
+    },
+  ));
 }
-
